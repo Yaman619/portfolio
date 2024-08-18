0779094178b580210799850fa0bf5efe969e968e
Yaman619 <yamanalawa@hotmail.com>
2024-08-18 14:37:20 +0300

    uploading my work on this project for the first time

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="flex flex-col min-h-screen bg-secondary-dark">
+    <Navigation />
+    <RouterView />
+    <justified-grid />
+  </div>
+</template>
+
+<script setup>
+import { RouterView } from 'vue-router';
+import Navigation from './components/navigation.vue';
+import {justified-grid} from './components/grid.vue';
+</script>
+
+<style lang="scss" scoped>
+
+</style>
